67a3061e37df53bbd41805117f763acbc120cdf4
suryaicungs <111480037+suryaicungs@users.noreply.github.com>
2022-08-22 13:31:57 +0700

    Add files via upload

---
+++ python4-surya.py
@@ -0,0 +1,19 @@
+print("1. Persegi\n")
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+print("2. persegi panjang")
+
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar"))
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah:", luas_pp)
+
+print("3. segitiga")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2 
+print("luas segitiga adalah:", luas_s)
